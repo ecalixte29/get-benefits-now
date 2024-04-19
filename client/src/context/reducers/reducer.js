a8c931ca022d403f8fc2c381e51c187736357b71
devhuzaifa98 <huzaiffajalil@gmail.com>
2024-04-19 18:17:01 +0500

    client: move insurance type from home to form

--- client/src/context/reducers/reducer.js
+++ client/src/context/reducers/reducer.js
@@ -1,16 +1,39 @@
 export const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_FORM':
-            return { ...state, form: action.payload }
-        case 'NEXT_STEP':
-            return { ...state, currentStep: state.currentStep + 1, currentSubStep: 0 }
-        case 'PREVIOUS_STEP':
-            return { ...state, currentStep: state.currentStep - 1, currentSubStep: state.form[state.currentStep - 1].subSteps.length - 1 }
-        case 'NEXT_SUBSTEP':
-            return { ...state, currentSubStep: state.currentSubStep + 1 }
-        case 'PREVIOUS_SUBSTEP':
-            return { ...state, currentSubStep: state.currentSubStep - 1 }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_FORM":
+      return { ...state, form: action.payload };
+    case "NEXT_STEP":
+      const nextSubSteps = state.form[state.currentStep + 1].subSteps.filter(
+        (step) =>
+          !step.dependency ||
+          step.dependency.values.includes(
+            state.form
+              .flatMap((subStep) => subStep.subSteps)
+              .flatMap((subStep) => subStep.fields)
+              .find((field) => field.name === step.dependency.name).value
+          )
+      );
+      const updatedForm = [...state.form];
+      updatedForm[state.currentStep + 1] = {
+        ...state.form[state.currentStep + 1],
+        subSteps: nextSubSteps,
+      };
+      return {
+        form: updatedForm,
+        currentStep: state.currentStep + 1,
+        currentSubStep: 0,
+      };
+    case "PREVIOUS_STEP":
+      return {
+        ...state,
+        currentStep: state.currentStep - 1,
+        currentSubStep: state.form[state.currentStep - 1].subSteps.length - 1,
+      };
+    case "NEXT_SUBSTEP":
+      return { ...state, currentSubStep: state.currentSubStep + 1 };
+    case "PREVIOUS_SUBSTEP":
+      return { ...state, currentSubStep: state.currentSubStep - 1 };
+    default:
+      return state;
+  }
+};
--- client/src/pages/home/Hero.js
+++ client/src/pages/home/Hero.js
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Hero.scss";
-import { MdFamilyRestroom, MdMan } from "react-icons/md";
-import { RiParentFill } from "react-icons/ri";
-import RadioButtonCard from "../../components/form/RadioButtonCard";
-import { ImManWoman } from "react-icons/im";
 import { useNavigate } from "react-router-dom";
 import TextInput from "../../components/form/TextInput";
 import { SecondaryButton } from "../../components/buttons";
 
 const Hero = () => {
   const navigate = useNavigate();
-  const [zipEntered, setZipEntered] = useState(false);
-  const [zip, setZip] = useState("")
 
   return (
     <section
@@ -28,57 +22,19 @@ const Hero = () => {
         <h1 className="text-1xl md:text-6xl mx-15 font-semibold">
           Insurance made easy for everyone
         </h1>
-        {zipEntered ? (
-          <>
-            <p className="max-w-[700px] mx-auto mb-8">
-              Who would you like to insure?
-            </p>
-            <div className="flex justify-center space-x-2s flex-wrap">
-              <RadioButtonCard
-                id="single"
-                name="hero-type"
-                label="Single"
-                icon={MdMan}
-                size={50}
-                to="/form"
-              />
-              <RadioButtonCard
-                id="couple"
-                name="hero-type"
-                label="couple"
-                icon={ImManWoman}
-                size={50}
-                to={"/form?type=couple"}
-              />
-              <RadioButtonCard
-                id="family"
-                name="hero-type"
-                label="family"
-                icon={MdFamilyRestroom}
-                size={50}
-                to={"/form?type=family"}
-              />
-              <RadioButtonCard
-                id="single-parent"
-                name="hero-type"
-                label="single parent"
-                icon={RiParentFill}
-                size={50}
-                to={"/form?type=single-parent"}
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <p className="max-w-[700px] mx-auto mb-8">
-              Enter your zip code to see if you qualify
-            </p>
-            <div className="gap-x-5 flex flex-row items-start justify-center">
-              <TextInput width={'lg:w-[300px] w-[200px]'} placeholder="123456" label={""} onChange={() => {}} className={'w-[300px]'}  />
-              <SecondaryButton onClick={() => setZipEntered(true)} text={"Next"} />
-            </div>
-          </>
-        )}
+        <p className="max-w-[700px] mx-auto mb-8">
+          Enter your zip code to see if you qualify
+        </p>
+        <div className="gap-x-5 flex flex-row items-start justify-center">
+          <TextInput
+            width={"lg:w-[300px] w-[200px]"}
+            placeholder="123456"
+            label={""}
+            onChange={() => {}}
+            className={"w-[300px]"}
+          />
+          <SecondaryButton onClick={() => navigate('/form')} text={"Next"} />
+        </div>
       </div>
     </section>
   );
