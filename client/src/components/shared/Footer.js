58498aaf7fad46cdddecf8725fec194f35faccb3
Huzaifa Jalil <huzaiffajalil@gmail.com>
2024-04-26 17:51:13 +0500

    Test autodeploy

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                     <img src={LogoImg} alt="" width={30} />
                     <h1 className='font-josefin lowercase leading-0 text-white text-xl text-center md:text-2xl font-bold cursor-pointer mb-3 sm:mb-0'>benefits-rite-now</h1>
                 </div>
-                <p>Copyrights © 2024</p>
+                <p>Copyrights © 2024.</p>
             </div>
         </div>
 
